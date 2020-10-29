e4e64cb3c23dd5a1865123361dc07265c3c7e238
dbainbri <dbainbri@ciena.com>
2020-10-29 03:04:41 +0000

    add version check

---
+++ version_check.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+VERSION=${VERSION:-$(cat VERSION)}
+MAJOR=$(echo $VERSION | cut -d. -f1)
+MINOR=$(echo $VERSION | cut -d. -f2)
+PATCH=$(echo $VERSION | cut -d. -f3)
+NOTE=
+
+if [[ $PATCH =~ [0-9]+-.* ]]; then
+    NOTE=$(echo $PATCH | cut -d- -f2)
+    PATCH=$(echo $PATCH | cut -d- -f1)
+fi
+
+CHECK_VERSION=$MAJOR.$MINOR.$PATCH
+
+LARGEST=$(git tag -l | sort -V -r | head -1)
+GOOD=$(echo -e "$CHECK_VERSION\n$LARGEST" | sort -V -r | head -1)
+if [[ $GOOD == $LARGEST ]]; then
+    exit 2
+fi
